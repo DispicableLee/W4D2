817cb5f7a2867d9fe837b85b1bc1264a1cede749
“Eltion <ebehrami01@gmail.com>
2023-09-12 15:03:51 -0400

    method test

--- practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
@@ -7,21 +7,55 @@ class Employee
         @title = title 
         @salary = salary 
         @boss = boss
+        @
     end 
 
     def bonus(multiplier)
-        if @title != manager
+        if self.
             @salary += @salary * multiplier
         else 
-            
-
-
+            salary_before_multiplier = self.summed_salaries
+            salary_before_multiplier * multiplier
+            #sum = 0 
+            # if employee != manager, sum += employee.salary to sum 
+                #if it is a manager, sum + = (manager.bonus) / multiplier (add its employees salary and multiply by multiplier) 
         end 
     end 
 
+    def boss=(new_boss)
+        if @boss != nil 
+            @boss.managed_employees.delete(self)
+        end 
+        if new_boss.nil? 
+            @boss = nil 
+        else 
+            @boss = new_boss
+            new_boss.managed_employees << self unless new_boss.managed_employees.include?(self)
+        end 
 
+    end 
 
-
+    def summed_salaries
+        current_salary = self.salary 
+        managed_employees.each do |employee|
+            if employee.managed_employees.nil? 
+                current_salary += employee.salary 
+            else 
+                current_salary += employee.summed_salaries
+            end 
+        end 
+        current_salary
+    end 
 
 
 end 
+
+
+e = Employee.new("Eltion", "chef", "1000000")
+a = Employee.new("Rob", "waiter", "78000", e)
+b = Employee.new("Bob", "busboy", "12,000", a)
+c = Employee.new("Jack", "waiter", "10,000", a)
+
+p a.boss
+
+p e.bonus(5)
