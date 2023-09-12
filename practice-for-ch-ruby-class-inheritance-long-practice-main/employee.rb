60240d3d3e772b77cba6faeb16a6379c453f4638
DispicableLee <lee.robert053@gmail.com>
2023-09-12 15:35:18 -0400

    done

--- practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
@@ -1,25 +1,37 @@
 class Employee
 
-    attr_accessor :name, :title, :salary, :boss
+    attr_accessor :name, :title, :salary, :boss, :managed_employees
 
     def initialize(name, title, salary, boss = nil)
         @name = name 
         @title = title 
         @salary = salary 
         @boss = boss
-        @
+        @managed_employees = []
     end 
 
     def bonus(multiplier)
-        if self.
-            @salary += @salary * multiplier
-        else 
-            salary_before_multiplier = self.summed_salaries
-            salary_before_multiplier * multiplier
-            #sum = 0 
-            # if employee != manager, sum += employee.salary to sum 
-                #if it is a manager, sum + = (manager.bonus) / multiplier (add its employees salary and multiply by multiplier) 
-        end 
+        p "hi"
+        # if self.managed_employees.empty?
+        #     p "no"
+        #     @salary += @salary * multiplier
+        # else 
+        #     salary_before_multiplier = self.summed_salaries
+        #     p "salary_before_multiplier: #{salary_before_multiplier}"
+        #     salary_before_multiplier * multiplier
+        # end 
+        before_bonus = 0
+        if self.managed_employees.empty?
+            
+        else
+        end
+
+
+
+
+
+
+
     end 
 
     def boss=(new_boss)
@@ -35,27 +47,35 @@ class Employee
 
     end 
 
-    def summed_salaries
-        current_salary = self.salary 
-        managed_employees.each do |employee|
-            if employee.managed_employees.nil? 
-                current_salary += employee.salary 
-            else 
-                current_salary += employee.summed_salaries
-            end 
-        end 
-        current_salary
-    end 
+    # def summed_salaries
+    #     current_salary = self.salary
+    #     managed_employees.each do |employee|
+    #         if employee.managed_employees.empty? 
+    #             current_salary += employee.salary 
+    #         else 
+    #             current_salary += employee.summed_salaries
+    #         end 
+    #     end 
+    #     current_salary - self.salary
+    # end 
 
 
 end 
 
 
-e = Employee.new("Eltion", "chef", "1000000")
-a = Employee.new("Rob", "waiter", "78000", e)
-b = Employee.new("Bob", "busboy", "12,000", a)
-c = Employee.new("Jack", "waiter", "10,000", a)
+# e = Employee.new("Eltion", "chef", 1000000)
+# a = Employee.new("Rob", "waiter", 78000, e)
+# b = Employee.new("Bob", "busboy", 12000, a)
+# c = Employee.new("Jack", "waiter", 10000, a)
 
-p a.boss
+# a.boss=e
 
-p e.bonus(5)
+# b.boss=a
+# c.boss=a
+
+# p e.summed_salaries
+
+# p e.bonus(5)
+
+# a.managed_employees.each {|e| p e.name}
+
--- practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
@@ -1,16 +1,39 @@
 require_relative "employee"
 
 class Manager < Employee
+    attr_accessor :name, :title, :salary, :boss, :managed_employees
 
-    def initialize(name, title, salary, boss, managed_employees)
+    def initialize(name, title, salary, boss=nil, managed_employees=[])
         super(name, title, salary, boss)
-        @managed_employees = []
-
+        @managed_employees = managed_employees
     end 
 
 
-
+    def manager_bonus(multiplier=1)
+        temp = 0
+        self.managed_employees.each do |employee|
+            if employee.managed_employees.empty?
+                temp += employee.bonus
+            else
+                temp+= (employee.manager_bonus(multiplier)/multiplier)
+            end
+        end
+        temp*multiplier + @salary
+    end
 
 
 
 end 
+
+e = Manager.new("Eltion", "chef", 1000000)
+
+a = Manager.new("Rob", "waiter", 78000,e)
+b = Manager.new("Bob", "busboy", 12000,a)
+c = Manager.new("Jack", "waiter", 10000,a)
+
+
+
+
+
+
+p e.managed_employees
