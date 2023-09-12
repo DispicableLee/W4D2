67cccd01bb9558b1cce465e60cb0ed855e75547a
“Eltion <ebehrami01@gmail.com>
2023-09-12 14:54:09 -0400

    bonus method testing

--- practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
@@ -1,1 +1,27 @@
+class Employee
 
+    attr_accessor :name, :title, :salary, :boss
+
+    def initialize(name, title, salary, boss = nil)
+        @name = name 
+        @title = title 
+        @salary = salary 
+        @boss = boss
+    end 
+
+    def bonus(multiplier)
+        if @title != manager
+            @salary += @salary * multiplier
+        else 
+            
+
+
+        end 
+    end 
+
+
+
+
+
+
+end 
