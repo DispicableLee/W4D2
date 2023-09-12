5eea3fba9d11b44cd43f0d53301377f4ee6f2b4f
“Eltion <ebehrami01@gmail.com>
2023-09-12 14:54:09 -0400

    bonus method testing

--- practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
@@ -1,1 +1,16 @@
+require_relative "employee"
 
+class Manager < Employee
+
+    def initialize(name, title, salary, boss, managed_employees)
+        super(name, title, salary, boss)
+        @managed_employees = []
+
+    end 
+
+
+
+
+
+
+end 
